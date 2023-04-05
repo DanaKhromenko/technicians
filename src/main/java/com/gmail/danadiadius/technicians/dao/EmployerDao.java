f168e338926a7f26cd6052c0778ed316a91b58c0
DanaKhromenko <danadiadius@gmail.com>
2023-04-05 17:22:39 -0600

    Added employer service, service impl and dao

---
+++ src/main/java/com/gmail/danadiadius/technicians/dao/EmployerDao.java
@@ -0,0 +1,7 @@
+package com.gmail.danadiadius.technicians.dao;
+
+import com.gmail.danadiadius.technicians.model.Employer;
+import com.gmail.danadiadius.technicians.service.GenericService;
+
+public interface EmployerDao extends GenericService<Employer> {
+}
---
+++ src/main/java/com/gmail/danadiadius/technicians/service/EmployerService.java
@@ -0,0 +1,6 @@
+package com.gmail.danadiadius.technicians.service;
+
+import com.gmail.danadiadius.technicians.model.Employer;
+
+public interface EmployerService extends GenericService<Employer> {
+}
