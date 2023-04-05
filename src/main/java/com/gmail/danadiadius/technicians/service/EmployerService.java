14b93c082719e8c13d2cb9e457bfdc0b4a28f092
DanaKhromenko <danadiadius@gmail.com>
2023-04-05 17:41:14 -0600

    Added an option to find employer by e-mail

--- src/main/java/com/gmail/danadiadius/technicians/service/EmployerService.java
+++ src/main/java/com/gmail/danadiadius/technicians/service/EmployerService.java
@@ -2,5 +2,8 @@ package com.gmail.danadiadius.technicians.service;
 
 import com.gmail.danadiadius.technicians.model.Employer;
 
+import java.util.Optional;
+
 public interface EmployerService extends GenericService<Employer> {
+    Optional<Employer> findByEmail(String email);
 }
