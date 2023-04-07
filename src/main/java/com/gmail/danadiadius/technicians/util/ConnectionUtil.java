98b72451c09aa049225b8fba28ad09c6ddaead6a
DanaKhromenko <danadiadius@gmail.com>
2023-04-07 00:57:19 -0600

    Updated getConnection() in ConnectionUtil

--- src/main/java/com/gmail/danadiadius/technicians/util/ConnectionUtil.java
+++ src/main/java/com/gmail/danadiadius/technicians/util/ConnectionUtil.java
@@ -3,10 +3,9 @@ package com.gmail.danadiadius.technicians.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/technicians?useUnicode=true" +
+    private static final String URL = "jdbc:mysql://localhost:3306/technicians_schema?useUnicode=true" +
             "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "4570810"; // Note: in real project secure data cannot be stored this way!
@@ -21,11 +20,8 @@ public class ConnectionUtil {
     }
 
     public static Connection getConnection() {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
         try {
-            return DriverManager.getConnection(URL, dbProperties);
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB ", e);
         }
