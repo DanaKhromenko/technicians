9fc541454a8e7a48bb69f4a17ac97b57e984d0dd
DanaKhromenko <danadiadius@gmail.com>
2023-04-06 22:30:51 -0600

    Added DataProcessingException

---
+++ src/main/java/com/gmail/danadiadius/technicians/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.gmail.danadiadius.technicians.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
