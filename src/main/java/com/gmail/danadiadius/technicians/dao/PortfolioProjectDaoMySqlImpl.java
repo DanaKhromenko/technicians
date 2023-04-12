ab8330b7a9ac952d758fb3eccd651c6e815f27f6
DanaKhromenko <danadiadius@gmail.com>
2023-04-11 20:20:45 -0600

    Updated dao implementations

--- src/main/java/com/gmail/danadiadius/technicians/dao/PortfolioProjectDaoMySqlImpl.java
+++ src/main/java/com/gmail/danadiadius/technicians/dao/PortfolioProjectDaoMySqlImpl.java
@@ -4,7 +4,7 @@ import com.gmail.danadiadius.technicians.exception.DataProcessingException;
 import com.gmail.danadiadius.technicians.lib.Dao;
 import com.gmail.danadiadius.technicians.lib.Inject;
 import com.gmail.danadiadius.technicians.model.PortfolioProject;
-import com.gmail.danadiadius.technicians.model.Tool;
+import com.gmail.danadiadius.technicians.model.Technician;
 import com.gmail.danadiadius.technicians.service.ToolService;
 import com.gmail.danadiadius.technicians.util.ConnectionUtil;
 
@@ -16,36 +16,27 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Dao
 public class PortfolioProjectDaoMySqlImpl implements PortfolioProjectDao {
     @Inject
     private ToolService toolService;
 
-    private static final int PARAMETER_SHIFT = 2; // helps to calculate the query parameter number
-
     @Override
     public PortfolioProject create(PortfolioProject portfolioProject) {
-        String query = "INSERT INTO portfolio_projects (name, short_project_description, " +
-                "detailed_project_description, source_code_url, interactive_result_url, picture_url) " +
-                "VALUES (?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO portfolio_projects (name, description, url, picture) VALUES (?, ?, ?, ?)";
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement preparedStatement
                      = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, portfolioProject.getName());
-            preparedStatement.setString(2, portfolioProject.getShortProjectDescription());
-            preparedStatement.setString(3, portfolioProject.getDetailedProjectDescription());
-            preparedStatement.setString(4, portfolioProject.getSourceCodeUrl());
-            preparedStatement.setString(5, portfolioProject.getInteractiveResultUrl());
-            preparedStatement.setString(6, portfolioProject.getPictureUrl());
+            preparedStatement.setString(2, portfolioProject.getDescription());
+            preparedStatement.setString(3, portfolioProject.getUrl());
+            preparedStatement.setBytes(4, portfolioProject.getPicture());
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 portfolioProject.setId(resultSet.getObject(1, Long.class));
             }
-
-            addPortfolioProjectAndToolsRelations(portfolioProject);
             return portfolioProject;
         } catch (SQLException e) {
             throw new DataProcessingException("Could not create portfolio project " + portfolioProject, e);
@@ -55,67 +46,58 @@ public class PortfolioProjectDaoMySqlImpl implements PortfolioProjectDao {
     @Override
     public Optional<PortfolioProject> get(Long id) {
         String selectQuery = "SELECT * FROM portfolio_projects WHERE id = ? AND is_deleted = FALSE";
-        PortfolioProject portfolioProject = null;
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
+            PortfolioProject portfolioProject = null;
             if (resultSet.next()) {
                 portfolioProject = parsePortfolioProjectFromResultSet(resultSet);
+                portfolioProject.setTools(toolService.getAllToolsByPortfolioProject(id));
             }
+            return Optional.ofNullable(portfolioProject);
         } catch (SQLException e) {
-            throw new DataProcessingException("Could not get a portfolio project by id: " + id, e);
+            throw new DataProcessingException("Could not get a portfolio project by id " + id, e);
         }
-        if (portfolioProject != null) {
-            portfolioProject.setTools(toolService.getAllByPortfolioProject(portfolioProject.getId()));
-        }
-        return Optional.ofNullable(portfolioProject);
     }
 
     @Override
     public List<PortfolioProject> getAll() {
         String query = "SELECT * FROM portfolio_projects WHERE is_deleted = FALSE";
-        List<PortfolioProject> portfolioProjects = new ArrayList<>();
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            List<PortfolioProject> portfolioProjects = new ArrayList<>();
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                portfolioProjects.add(parsePortfolioProjectFromResultSet(resultSet));
+                PortfolioProject portfolioProject = parsePortfolioProjectFromResultSet(resultSet);
+                portfolioProject.setTools(toolService.getAllToolsByPortfolioProject(portfolioProject.getId()));
+                portfolioProjects.add(portfolioProject);
             }
+            return portfolioProjects;
         } catch (SQLException e) {
             throw new DataProcessingException("Could not get all portfolio projects.", e);
         }
-
-        // Add tools to the Portfolio Projects
-        portfolioProjects.forEach(portfolioProject -> portfolioProject.setTools(
-                toolService.getAllByPortfolioProject(portfolioProject.getId())));
-        return portfolioProjects;
     }
 
     @Override
     public PortfolioProject update(PortfolioProject portfolioProject) {
-        String selectQuery = "UPDATE portfolio_projects SET name = ?, short_project_description = ?, " +
-                "detailed_project_description = ?, source_code_url = ?, interactive_result_url = ?, picture_url = ? " +
+        String query = "UPDATE portfolio_projects SET name = ?, description = ?, url = ?, picture = ? " +
                 "WHERE id = ? AND is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, portfolioProject.getName());
-            preparedStatement.setString(2, portfolioProject.getShortProjectDescription());
-            preparedStatement.setString(3, portfolioProject.getDetailedProjectDescription());
-            preparedStatement.setString(4, portfolioProject.getSourceCodeUrl());
-            preparedStatement.setString(5, portfolioProject.getInteractiveResultUrl());
-            preparedStatement.setString(6, portfolioProject.getPictureUrl());
-            preparedStatement.setLong(7, portfolioProject.getId());
+            preparedStatement.setString(2, portfolioProject.getDescription());
+            preparedStatement.setString(3, portfolioProject.getUrl());
+            preparedStatement.setBytes(4, portfolioProject.getPicture());
+            preparedStatement.setLong(5, portfolioProject.getId());
             preparedStatement.executeUpdate();
+
+            toolService.updatePortfolioProjectsTools(portfolioProject);
+
+            return portfolioProject;
         } catch (SQLException e) {
             throw new DataProcessingException("Could not update portfolio project " + portfolioProject, e);
         }
-
-        // Update Portfolio Project's Tools
-        removePortfolioProjectAndToolsOutdatedRelations(portfolioProject);
-        addPortfolioProjectAndToolsRelations(portfolioProject);
-
-        return portfolioProject;
     }
 
     @Override
@@ -131,84 +113,70 @@ public class PortfolioProjectDaoMySqlImpl implements PortfolioProjectDao {
     }
 
     @Override
-    public List<PortfolioProject> getAllByTechnician(Long TechnicianId) {
-        String query = "SELECT pp.* " +
-                "FROM portfolio_projects pp " +
-                " INNER JOIN technicians t on pp.technician_id = t.id " +
-                "WHERE t.id = ? AND pp.is_deleted = FALSE;";
-        List<PortfolioProject> portfolioProjects = new ArrayList<>();
+    public List<PortfolioProject> getAllByTechnician(Long technicianId) {
+        String selectQuery = "SELECT * FROM portfolio_projects WHERE is_deleted = FALSE AND technician_id = ?";
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+            List<PortfolioProject> portfolioProjects = new ArrayList<>();
+            preparedStatement.setLong(1, technicianId);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                portfolioProjects.add(parsePortfolioProjectFromResultSet(resultSet));
+                PortfolioProject portfolioProject = parsePortfolioProjectFromResultSet(resultSet);
+                portfolioProject.setTools(toolService.getAllToolsByPortfolioProject(portfolioProject.getId()));
+                portfolioProjects.add(portfolioProject);
             }
+            return portfolioProjects;
         } catch (SQLException e) {
-            throw new DataProcessingException("Could not get all portfolio projects.", e);
-        }
-
-        // Add tools to the Portfolio Projects
-        portfolioProjects.forEach(portfolioProject -> portfolioProject.setTools(
-                toolService.getAllByPortfolioProject(portfolioProject.getId())));
-        return portfolioProjects;
-    }
-
-    private void addPortfolioProjectAndToolsRelations(PortfolioProject portfolioProject) {
-        Long portfolioProjectId = portfolioProject.getId();
-        List<Tool> tools = portfolioProject.getTools();
-        if (tools.size() == 0) {
-            return;
-        }
-
-        String query = "INSERT INTO portfolio_projects_tools (portfolio_project_id, tool_id) VALUES "
-                // Note: instead of drivers add "(?, ?), " string to set IDs in the loop below
-                + tools.stream().map(tool -> "(?, ?)").collect(Collectors.joining(", "))
-                + " ON DUPLICATE KEY UPDATE portfolio_project_id = portfolio_project_id";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement
-                     = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-            for (int i = 0; i < tools.size(); i++) {
-                Tool tool = tools.get(i);
-                preparedStatement.setLong((i * PARAMETER_SHIFT) + 1, portfolioProjectId);
-                preparedStatement.setLong((i * PARAMETER_SHIFT) + 2, tool.getId());
-            }
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new DataProcessingException("Could not store relations between PortfolioProject " + portfolioProject +
-                    " and Tools " + tools, e);
+            throw new DataProcessingException("Could not get portfolio projects by technician id " + technicianId, e);
         }
     }
 
-    private void removePortfolioProjectAndToolsOutdatedRelations(PortfolioProject portfolioProject) {
-        Long portfolioProjectId = portfolioProject.getId();
-        List<Tool> toolExceptions = portfolioProject.getTools();
-        int size = toolExceptions.size();
-        Long dummyIdForRepeatMethod = 0L;
-        String insertQuery = "DELETE FROM portfolio_projects_tools WHERE portfolio_project_id = ? "
-                + "AND NOT portfolio_projects_tools.tool_id IN (" + dummyIdForRepeatMethod + ", ?".repeat(size) + ");";
+    @Override
+    public void updateTechnicianPortfolioProjects(Technician technician) {
+        List<PortfolioProject> portfolioProjectsCurrent = technician.getPortfolioProjects();
+        List<PortfolioProject> portfolioProjectsFromDB = getAllByTechnician(technician.getId());
+
+        // Remove from MySQL DB portfolio projects that are no longer belong to technician
+        List<PortfolioProject> portfolioProjectsToRemoveFromDB = new ArrayList<>(portfolioProjectsFromDB);
+        if (portfolioProjectsCurrent != null) {
+            portfolioProjectsToRemoveFromDB.removeAll(portfolioProjectsCurrent);
+        }
+        portfolioProjectsToRemoveFromDB.forEach(pp -> delete(pp.getId()));
+
+        // Add to MySQL DB portfolio projects that are now belong to technician
+        // and add relations between technician and portfolio projects
+        if (portfolioProjectsCurrent != null) {
+            List<PortfolioProject> portfolioProjectsToAddToDB = new ArrayList<>(portfolioProjectsCurrent);
+            portfolioProjectsToAddToDB.removeAll(portfolioProjectsFromDB);
+            portfolioProjectsToAddToDB.forEach(pp -> {
+                create(pp);
+                addPortfolioProjectToTechnician(technician.getId(), pp.getId());
+            });
+        }
+    }
+
+    @Override
+    public void addPortfolioProjectToTechnician(Long technicianId, Long portfolioProjectId) {
+        String query = "UPDATE portfolio_projects SET technician_id = ? WHERE id = ?";
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            preparedStatement.setLong(1, portfolioProjectId);
-            for (int i = 0; i < size; i++) {
-                preparedStatement.setLong((i) + PARAMETER_SHIFT, toolExceptions.get(i).getId());
-            }
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, technicianId);
+            preparedStatement.setLong(2, portfolioProjectId);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            throw new DataProcessingException("Could not remove outdated tools from Portfolio Project " +
-                    portfolioProject, e);
+            throw new DataProcessingException("Could not add portfolio project with id " + portfolioProjectId +
+                    " to technician with id " + technicianId, e);
         }
     }
 
     private PortfolioProject parsePortfolioProjectFromResultSet(ResultSet resultSet) throws SQLException {
-        Long portfolioProjectId = resultSet.getObject("id", Long.class);
-        String name = resultSet.getString("name");
-        String shortProjectDescription = resultSet.getString("short_project_description");
-        String detailedProjectDescription = resultSet.getString("detailed_project_description");
-        String sourceCodeUrl = resultSet.getString("source_code_url");
-        String interactiveResultUrl = resultSet.getString("interactive_result_url");
-        String pictureUrl = resultSet.getString("picture_url");
+        PortfolioProject portfolioProject = new PortfolioProject();
+        portfolioProject.setId(resultSet.getObject("id", Long.class));
+        portfolioProject.setName(resultSet.getString("name"));
+        portfolioProject.setDescription(resultSet.getString("description"));
+        portfolioProject.setUrl(resultSet.getString("url"));
+        portfolioProject.setPicture(resultSet.getBytes("picture"));
 
-        return new PortfolioProject(portfolioProjectId, name, shortProjectDescription,
-                detailedProjectDescription, sourceCodeUrl, interactiveResultUrl, pictureUrl, null);
+        return portfolioProject;
     }
 }
--- src/main/java/com/gmail/danadiadius/technicians/dao/TechnicianDaoMySqlImpl.java
+++ src/main/java/com/gmail/danadiadius/technicians/dao/TechnicianDaoMySqlImpl.java
@@ -3,7 +3,6 @@ package com.gmail.danadiadius.technicians.dao;
 import com.gmail.danadiadius.technicians.exception.DataProcessingException;
 import com.gmail.danadiadius.technicians.lib.Dao;
 import com.gmail.danadiadius.technicians.lib.Inject;
-import com.gmail.danadiadius.technicians.model.PortfolioProject;
 import com.gmail.danadiadius.technicians.model.Technician;
 import com.gmail.danadiadius.technicians.model.User;
 import com.gmail.danadiadius.technicians.service.PortfolioProjectService;
@@ -16,11 +15,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Currency;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Dao
 public class TechnicianDaoMySqlImpl implements TechnicianDao {
@@ -30,9 +27,6 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
     @Inject
     private UserService userService;
 
-    private static final int ZERO_PLACEHOLDER = 0;
-    private static final int PARAMETER_SHIFT = 2;
-
     @Override
     public Technician create(Technician technician) {
         String query = "INSERT INTO technicians (user_id, open_to_work, desired_position, desired_annual_salary, " +
@@ -40,7 +34,6 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement preparedStatement
                      = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-            // Store technician to DB
             preparedStatement.setLong(1, technician.getId());
             preparedStatement.setBoolean(2, technician.isOpenToWork());
             preparedStatement.setString(3, technician.getDesiredPosition());
@@ -55,14 +48,7 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
                 technician.setId(resultSet.getObject(1, Long.class));
             }
 
-            // Store technician's portfolio projects to DB
-            if (technician.getPortfolioProjects() != null) {
-                for (PortfolioProject portfolioProject : technician.getPortfolioProjects()) {
-                    PortfolioProject portfolioProjectFromDB = portfolioProjectService.create(portfolioProject);
-                    portfolioProject.setId(portfolioProjectFromDB.getId());
-                }
-            }
-
+            portfolioProjectService.updateTechnicianPortfolioProjects(technician);
             return technician;
         } catch (SQLException e) {
             throw new DataProcessingException("Could not create technician " + technician, e);
@@ -79,7 +65,8 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
             ResultSet resultSet = preparedStatement.executeQuery();
             Technician technician = null;
             if (resultSet.next()) {
-                technician = getTechnician(resultSet);
+                technician = parseTechnicianFromResultSet(resultSet);
+                technician.setPortfolioProjects(portfolioProjectService.getAllByTechnician(id));
             }
             return Optional.ofNullable(technician);
         } catch (SQLException e) {
@@ -95,7 +82,7 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                Technician technician = getTechnician(resultSet);
+                Technician technician = parseTechnicianFromResultSet(resultSet);
                 technician.setPortfolioProjects(portfolioProjectService.getAllByTechnician(technician.getId()));
                 technicians.add(technician);
             }
@@ -117,8 +104,7 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
                      = getPreparedStatementWithTechnicianData(connection.prepareStatement(query), technician)) {
             preparedStatement.executeUpdate();
 
-            deleteAllPortfolioProjectsExceptList(technician);
-            insertAllPortfolioProjects(technician);
+            portfolioProjectService.updateTechnicianPortfolioProjects(technician);
 
             return technician;
         } catch (SQLException e) {
@@ -128,33 +114,29 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
 
     @Override
     public boolean delete(Long id) {
+        // Due to soft delete strategy there is no need to delete data from `technicians` MySQL table
         return userService.delete(id);
     }
 
-    private Technician getTechnician(ResultSet resultSet) throws SQLException {
-        Long id = resultSet.getObject("id", Long.class);
-        String name = resultSet.getString("name");
-        String email = resultSet.getString("email");
-        String password = resultSet.getString("password");
-        String description = resultSet.getString("description");
+    private Technician parseTechnicianFromResultSet(ResultSet resultSet) throws SQLException {
+        Technician technician = new Technician();
 
-        boolean openToWork = resultSet.getBoolean("open_to_work");
-        String desiredPosition = resultSet.getString("desired_position");
-        int desiredAnnualSalary = resultSet.getInt("desired_annual_salary");
-        String strAnnualSalaryCurrency = resultSet.getString("annual_salary_currency");
-        Currency annualSalaryCurrency = null;
-        if (!strAnnualSalaryCurrency.isEmpty()) {
-            annualSalaryCurrency = Currency.getInstance(strAnnualSalaryCurrency);
-        }
+        // User attributes
+        technician.setId(resultSet.getObject("id", Long.class));
+        technician.setName(resultSet.getString("name"));
+        technician.setEmail(resultSet.getString("email"));
+        technician.setPassword(resultSet.getString("password"));
+        technician.setDescription(resultSet.getString("description"));
+        technician.setCountry(resultSet.getString("country"));
+        technician.setCity(resultSet.getString("city"));
+        technician.setPhone(resultSet.getString("phone"));
+        /* technician.setPicture(new byte[]{}); TODO: update with the real user picture data */
 
-        Technician technician = new Technician(openToWork, desiredPosition, desiredAnnualSalary, annualSalaryCurrency,
-                Collections.emptyList());
-
-        technician.setId(id);
-        technician.setName(name);
-        technician.setEmail(email);
-        technician.setPassword(password);
-        technician.setDescription(description);
+        // Technician attributes
+        technician.setOpenToWork(resultSet.getBoolean("open_to_work"));
+        technician.setDesiredPosition(resultSet.getString("desired_position"));
+        technician.setDesiredAnnualSalary(resultSet.getInt("desired_annual_salary"));
+        technician.setAnnualSalaryCurrency(Currency.getInstance(resultSet.getString("annual_salary_currency")));
 
         return technician;
     }
@@ -167,66 +149,5 @@ public class TechnicianDaoMySqlImpl implements TechnicianDao {
         preparedStatement.setString(4, technician.getAnnualSalaryCurrency().getCurrencyCode());
         preparedStatement.setLong(5, technician.getId());
         return preparedStatement;
-    }
-
-    private void deleteAllPortfolioProjectsExceptList(Technician technician) {
-        List<PortfolioProject> exceptions = technician.getPortfolioProjects();
-        int size = exceptions.size();
-        String insertQuery = "DELETE FROM technician_portfolio_project WHERE technician_id = ? "
-                + "AND NOT technician_portfolio_project.portfolio_project_id IN (" + ZERO_PLACEHOLDER
-                + ", ?".repeat(size) + ");";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            preparedStatement.setLong(1, technician.getId());
-            for (int i = 0; i < size; i++) {
-                PortfolioProject portfolioProject = exceptions.get(i);
-                preparedStatement.setLong((i) + PARAMETER_SHIFT, portfolioProject.getId());
-            }
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new DataProcessingException("Could not delete portfolio projects.", e);
-        }
-    }
-
-    private void insertAllPortfolioProjects(Technician technician) {
-        Long technicianId = technician.getId();
-        List<PortfolioProject> portfolioProjects = technician.getPortfolioProjects();
-        if (portfolioProjects.size() == 0) {
-            return;
-        }
-        String insertQuery = "INSERT INTO technician_portfolio_project (technician_id, portfolio_project_id) VALUES "
-                + portfolioProjects.stream()
-                .map(portfolioProject -> "(?, ?)")
-                .collect(Collectors.joining(", "))
-                + " ON DUPLICATE KEY UPDATE technician_id = technician_id";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            for (int i = 0; i < portfolioProjects.size(); i++) {
-                PortfolioProject portfolioProject = portfolioProjects.get(i);
-                preparedStatement.setLong((i * PARAMETER_SHIFT) + 1, technicianId);
-                preparedStatement.setLong((i * PARAMETER_SHIFT) + 2, portfolioProject.getId());
-            }
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new DataProcessingException("Could not insert portfolio projects " + portfolioProjects, e);
-        }
-    }
-
-    @Override
-    public Optional<Technician> findByEmail(String email) {
-        String query = "SELECT * FROM technicians t INNER JOIN users u ON t.user_id = u.id WHERE u.email = ? AND " +
-                "u.is_deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setString(1, email);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            Technician technician = null;
-            if (resultSet.next()) {
-                technician = getTechnician(resultSet);
-            }
-            return Optional.ofNullable(technician);
-        } catch (SQLException e) {
-            throw new DataProcessingException("Could not get technician by e-mail " + email, e);
-        }
     }
 }
