d2b0cf47d798a993137835437269e337f7954f5b
DanaKhromenko <danadiadius@gmail.com>
2023-04-11 20:20:34 -0600

    Updated dao interfaces

--- src/main/java/com/gmail/danadiadius/technicians/dao/EmployerDao.java
+++ src/main/java/com/gmail/danadiadius/technicians/dao/EmployerDao.java
@@ -2,8 +2,5 @@ package com.gmail.danadiadius.technicians.dao;
 
 import com.gmail.danadiadius.technicians.model.Employer;
 
-import java.util.Optional;
-
 public interface EmployerDao extends GenericDao<Employer> {
-    Optional<Employer> findByEmail(String email);
 }
--- src/main/java/com/gmail/danadiadius/technicians/dao/TechnicianDao.java
+++ src/main/java/com/gmail/danadiadius/technicians/dao/TechnicianDao.java
@@ -2,8 +2,5 @@ package com.gmail.danadiadius.technicians.dao;
 
 import com.gmail.danadiadius.technicians.model.Technician;
 
-import java.util.Optional;
-
 public interface TechnicianDao extends GenericDao<Technician> {
-    Optional<Technician> findByEmail(String email);
 }
