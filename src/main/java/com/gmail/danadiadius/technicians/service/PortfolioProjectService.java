1e04bc1ffa4b3b244b0ee487a486f2c942c63769
DanaKhromenko <danadiadius@gmail.com>
2023-04-11 20:20:15 -0600

    Updated service interfaces

--- src/main/java/com/gmail/danadiadius/technicians/service/PortfolioProjectService.java
+++ src/main/java/com/gmail/danadiadius/technicians/service/PortfolioProjectService.java
@@ -1,14 +1,14 @@
 package com.gmail.danadiadius.technicians.service;
 
 import com.gmail.danadiadius.technicians.model.PortfolioProject;
-import com.gmail.danadiadius.technicians.model.Tool;
+import com.gmail.danadiadius.technicians.model.Technician;
 
 import java.util.List;
 
 public interface PortfolioProjectService extends GenericService<PortfolioProject> {
     List<PortfolioProject> getAllByTechnician(Long technicianId);
 
-    void addToolToPortfolioProject(Tool tool, PortfolioProject portfolioProject);
+    void updateTechnicianPortfolioProjects(Technician technician);
 
-    void removeToolFromPortfolioProject(Tool tool, PortfolioProject portfolioProject);
+    void addPortfolioProjectToTechnician(Long technicianId, Long portfolioProjectId);
 }
--- src/main/java/com/gmail/danadiadius/technicians/service/ToolService.java
+++ src/main/java/com/gmail/danadiadius/technicians/service/ToolService.java
@@ -1,9 +1,12 @@
 package com.gmail.danadiadius.technicians.service;
 
+import com.gmail.danadiadius.technicians.model.PortfolioProject;
 import com.gmail.danadiadius.technicians.model.Tool;
 
 import java.util.List;
 
 public interface ToolService extends GenericService<Tool> {
-    List<Tool> getAllByPortfolioProject(Long portfolioProjectId);
+    List<Tool> getAllToolsByPortfolioProject(Long portfolioProjectId);
+
+    void updatePortfolioProjectsTools(PortfolioProject portfolioProject);
 }
